31da64b52e059233dd71aceb0d3392e55d0f8b7b
Leandro Ferreira <leandroferreira@cliente-200-135-90-20.wireless.ufsc.br>
2019-12-12 20:59:31 -0300

    aula template email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fea9c1b5ad237b',
+    pass: 'b2657c54593ab0',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
